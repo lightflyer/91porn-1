a43c968be3c99d55419db00a1508503f74c7a5cc
QiLin <qq512554339@gmail.com>
2021-07-19 14:32:52 +0800

    Add files via upload

---
+++ M3U8Downloader/M3U8 Downloader/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace M3U8_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Download = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox_forRegex = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.button_ForceStop = new System.Windows.Forms.Button();
+            this.button_OpenFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Download
+            // 
+            this.button_Download.BackColor = System.Drawing.Color.Silver;
+            this.button_Download.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.button_Download.FlatAppearance.BorderSize = 0;
+            this.button_Download.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(162)))), ((int)(((byte)(210)))));
+            resources.ApplyResources(this.button_Download, "button_Download");
+            this.button_Download.ForeColor = System.Drawing.Color.White;
+            this.button_Download.Name = "button_Download";
+            this.button_Download.UseVisualStyleBackColor = false;
+            this.button_Download.Click += new System.EventHandler(this.button_Download_Click);
+            // 
+            // textBox_forRegex
+            // 
+            resources.ApplyResources(this.textBox_forRegex, "textBox_forRegex");
+            this.textBox_forRegex.Name = "textBox_forRegex";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.label8.Name = "label8";
+            // 
+            // ProgressBar
+            // 
+            resources.ApplyResources(this.ProgressBar, "ProgressBar");
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Step = 1;
+            // 
+            // button_ForceStop
+            // 
+            resources.ApplyResources(this.button_ForceStop, "button_ForceStop");
+            this.button_ForceStop.Name = "button_ForceStop";
+            this.button_ForceStop.UseVisualStyleBackColor = true;
+            this.button_ForceStop.Click += new System.EventHandler(this.button_ForceStop_Click);
+            // 
+            // button_OpenFolder
+            // 
+            resources.ApplyResources(this.button_OpenFolder, "button_OpenFolder");
+            this.button_OpenFolder.Name = "button_OpenFolder";
+            this.button_OpenFolder.UseVisualStyleBackColor = true;
+            this.button_OpenFolder.Click += new System.EventHandler(this.button_OpenFolder_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(162)))), ((int)(((byte)(210)))));
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(162)))), ((int)(((byte)(210)))));
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_OpenFolder);
+            this.Controls.Add(this.button_ForceStop);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_forRegex);
+            this.Controls.Add(this.button_Download);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Download;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox_forRegex;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.Button button_ForceStop;
+        private System.Windows.Forms.Button button_OpenFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
